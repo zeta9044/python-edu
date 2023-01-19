376ec6ff7610c1b5bdf4822a959a52e61e5b87d9
최강유(zeta) <zeta9044@gmail.com>
2023-01-19 10:31:36 +0900

    ...

--- nester/nester.py
+++
@@ -1,7 +0,0 @@
-def print_lol(the_list):
-    """중첩리스트의 값을 표시하는 함수"""
-    for each_item in the_list:
-        if isinstance(each_item, list):
-            print_lol(each_item)
-        else:
-            print(each_item)
--- nester/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='nester',
-    py_modules=['nester']
-)
